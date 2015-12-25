aea1251c24d63190fd7b35aeb848c076e0b156a9
Andy Lowry <andrew.lowry@laterooms.com>
2015-12-25 15:05:21 +0000

    Add testing helpers

---
+++ src/Ocelog.Testing/LoggingSpy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reflection;
+
+namespace Ocelog.Testing
+{
+    public class LoggingSpy
+    {
+        private List<LogEvent> _logEvents = new List<LogEvent>();
+
+        public Logger Logger { get; private set; }
+
+        public LoggingSpy()
+        {
+            Logger = new Logger(events => events.Subscribe(RecordLog));
+        }
+
+        public bool DidInfo(object content)
+        {
+            return _logEvents.Any(item => DidMatch(item.Content, content) && item.Level == LogLevel.Info);
+        }
+
+        public bool DidWarn(object content)
+        {
+            return _logEvents.Any(item => DidMatch(item.Content, content) && item.Level == LogLevel.Warn);
+        }
+
+        public bool DidError(object content)
+        {
+            return _logEvents.Any(item => DidMatch(item.Content, content) && item.Level == LogLevel.Error);
+        }
+
+        private void RecordLog(LogEvent logEvent)
+        {
+            _logEvents.Add(logEvent);
+        }
+
+        private bool DidMatch(object actualContent, object expectedContent)
+        {
+            if (actualContent.GetType().IsValueType || expectedContent.GetType().IsValueType)
+                return actualContent.Equals(expectedContent);
+
+            if (actualContent.GetType() == typeof(string) || expectedContent.GetType() == typeof(string))
+                return actualContent.Equals(expectedContent);
+
+            var expectedProperties = GetValueProperties(expectedContent);
+            var actualProperties = GetValueProperties(actualContent);
+
+            var matchingProperties = actualProperties
+                .Join(expectedProperties, left => left.Name, right => right.Name, (left, right) => new { left, right });
+
+            if (matchingProperties.Count() != expectedProperties.Count())
+                return false;
+
+            return matchingProperties.All(match => DidMatch(match.left.GetValue(actualContent), match.right.GetValue(expectedContent)));
+        }
+
+        private IEnumerable<PropertyInfo> GetValueProperties(object content)
+        {
+            return content.GetType().GetProperties()
+                .Where(prop => prop.CanRead
+                    && prop.GetAccessors().Any(access => access.ReturnType != typeof(void) && access.GetParameters().Length == 0));
+        }
+    }
+}
---
+++ test/Ocelog.Testing.Test/LoggingSpyTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ocelog.Testing.Test
+{
+    public class LoggingSpyTests
+    {
+        [Fact]
+        public void should_allow_asserting_on_info()
+        {
+            var logSpy = new LoggingSpy();
+            var expectedContent = new { Some = "Content" };
+
+            logSpy.Logger.Info(expectedContent);
+
+            Assert.True(logSpy.DidInfo(expectedContent));
+        }
+
+        [Fact]
+        public void should_allow_asserting_on_warnings()
+        {
+            var logSpy = new LoggingSpy();
+            var expectedContent = new { Some = "Content" };
+
+            logSpy.Logger.Warn(expectedContent);
+
+            Assert.True(logSpy.DidWarn(expectedContent));
+        }
+
+        [Fact]
+        public void should_allow_asserting_on_errors()
+        {
+            var logSpy = new LoggingSpy();
+            var expectedContent = new { Some = "Content" };
+
+            logSpy.Logger.Error(expectedContent);
+
+            Assert.True(logSpy.DidError(expectedContent));
+        }
+
+        [Theory]
+        [InlineData("Content", "Context")]
+        [InlineData(12, 5)]
+        [InlineData(12, "Content")]
+        public void should_be_false_when_no_matching_log_found(object actual, object expected)
+        {
+            var logSpy = new LoggingSpy();
+            var content = new { Some = actual };
+
+            logSpy.Logger.Info(content);
+
+            Assert.False(logSpy.DidInfo(new { Some = expected }));
+        }
+
+        [Fact]
+        public void should_not_match_value_and_complex_type()
+        {
+            var logSpy = new LoggingSpy();
+            var content = new { Some = "Content" };
+
+            logSpy.Logger.Info(content);
+
+            Assert.False(logSpy.DidInfo(new { Some = new { Complex = false } }));
+        }
+
+        [Fact]
+        public void should_not_match_complex_and_value_type()
+        {
+            var logSpy = new LoggingSpy();
+            var content = new { Some = new { Complex = false } };
+
+            logSpy.Logger.Info(content);
+
+            Assert.False(logSpy.DidInfo(new { Some = "Content" }));
+        }
+
+        [Fact]
+        public void should_allow_matching_of_same_shape()
+        {
+            var logSpy = new LoggingSpy();
+            var content = new { Some = "Content" };
+
+            logSpy.Logger.Info(content);
+
+            Assert.True(logSpy.DidInfo(new { Some = "Content" }));
+        }
+
+        [Fact]
+        public void should_allow_actual_to_have_extra_content()
+        {
+            var logSpy = new LoggingSpy();
+            var content = new { Some = "Content", Extra = "Content" };
+
+            logSpy.Logger.Info(content);
+
+            Assert.True(logSpy.DidInfo(new { Some = "Content" }));
+        }
+
+        [Fact]
+        public void should_allow_actual_to_have_extra_content_at_any_depth()
+        {
+            var logSpy = new LoggingSpy();
+            var content = new { Some = "Content", Extra = new { Content = "blah", Extra = true } };
+
+            logSpy.Logger.Info(content);
+
+            Assert.True(logSpy.DidInfo(new { Some = "Content", Extra = new { Content = "blah" } }));
+        }
+
+        [Fact]
+        public void should_not_allow_expected_to_have_extra_content_at_any_depth()
+        {
+            var logSpy = new LoggingSpy();
+            var content = new { Some = "Content", Extra = new { Content = "blah" } };
+
+            logSpy.Logger.Info(content);
+
+            Assert.False(logSpy.DidInfo(new { Some = "Content", Extra = new { Content = "blah", Extra = true } }));
+        }
+    }
+}
