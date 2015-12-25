f1071ff505bb774a781a86f7630efe24ccc0d611
Andy Lowry <andrew.lowry@laterooms.com>
2015-12-25 15:28:24 +0000

    Forgot to add testing assembly to package.

--- package/Package.bat
+++ package/Package.bat
@@ -7,5 +7,6 @@ mkdir content\controllers
 copy ..\src\Ocelog\bin\Debug\Ocelog.dll lib\net46
 copy ..\src\Ocelog.Formatting.Logstash\bin\Debug\Ocelog.Formatting.Logstash.dll lib\net46
 copy ..\src\Ocelog.Transport.UDP\bin\Debug\Ocelog.Transport.UDP.dll lib\net46
+copy ..\src\Ocelog.Testing\bin\Debug\Ocelog.Testing.dll lib\net46
 
 nuget pack
