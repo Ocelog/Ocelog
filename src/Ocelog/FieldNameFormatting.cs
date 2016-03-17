4b8d681ea03d3b9ce32f00ca22bad26a66c195f1
Andy Lowry <andrew.lowry@laterooms.com>
2016-03-17 14:49:09 +0000

    Add support for converting between casing styles

---
+++ src/Ocelog/FieldNameFormatting.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ocelog
+{
+    public static class FieldNameFormatting
+    {
+        public static Func<ProcessedLogEvent, ProcessedLogEvent> ToSnakeCase()
+        {
+            return (logEvent) => new ProcessedLogEvent() { Content = ToSnakeCase(logEvent.Content) };
+        }
+
+        public static Func<ProcessedLogEvent, ProcessedLogEvent> ToPascalCase()
+        {
+            return (logEvent) => new ProcessedLogEvent() { Content = ToPascalCase(logEvent.Content) };
+        }
+
+        public static Func<ProcessedLogEvent, ProcessedLogEvent> ToCamelCase()
+        {
+            return (logEvent) => new ProcessedLogEvent() { Content = ToCamelCase(logEvent.Content) };
+        }
+
+        private static Dictionary<string, object> ToSnakeCase(Dictionary<string, object> content)
+        {
+            return content.ToDictionary(pair => ToSnakeCase(pair.Key), pair => pair.Value);
+        }
+
+        private static string ToSnakeCase(string key)
+        {
+            return string.Join("_", Tokenize(key));
+        }
+
+        private static Dictionary<string, object> ToPascalCase(Dictionary<string, object> content)
+        {
+            return content.ToDictionary(pair => ToPascalCase(pair.Key), pair => pair.Value);
+        }
+
+        private static string ToPascalCase(string key)
+        {
+            return string.Concat(Tokenize(key).Select(word => CapitalizeFirstChar(word)));
+        }
+
+        private static Dictionary<string, object> ToCamelCase(Dictionary<string, object> content)
+        {
+            return content.ToDictionary(pair => ToCamelCase(pair.Key), pair => pair.Value);
+        }
+
+        private static string ToCamelCase(string key)
+        {
+            var tokens = Tokenize(key);
+            return string.Concat(tokens.Select((word, j) => j == 0 ? word : CapitalizeFirstChar(word)));
+        }
+
+        private static string CapitalizeFirstChar(string word)
+        {
+            return char.ToUpper(word.First()) + word.Substring(1);
+        }
+
+        private static string[] Tokenize(string key)
+        {
+            return string.Concat(key.Select((x, j) => ReplaceChar(x, j))).Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static string ReplaceChar(char x, int j)
+        {
+            if (j == 0)
+                return x.ToString().ToLower();
+            else if (char.IsUpper(x))
+                return " " + x.ToString().ToLower();
+            else if (char.IsWhiteSpace(x) || x == '_')
+                return " ";
+            else
+                return x.ToString();
+        }
+    }
+}
---
+++ test/Ocelog.Test/FieldNamesTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using System.Reactive.Linq;
+
+namespace Ocelog.Test
+{
+    public class FieldNamesTests
+    {
+        [Theory]
+        [InlineData("field", "field")]
+        [InlineData("Field", "field")]
+        [InlineData("Field Name", "field_name")]
+        [InlineData("FieldName", "field_name")]
+        [InlineData("fieldName", "field_name")]
+        [InlineData("field_Name", "field_name")]
+        [InlineData("field_name", "field_name")]
+        [InlineData("Field_name", "field_name")]
+        [InlineData("FieldName_Goes here", "field_name_goes_here")]
+    //    [InlineData("hotelID", "hotel_id")]
+        [InlineData("hotelId", "hotel_id")]
+        public void should_convert_fieldnames_to_snakecase(string originalFieldName, string finalFieldName)
+        {
+            const string valueToCheckFor = "valuetocheckfor";
+
+            var obj = new Dictionary<string, object> { { originalFieldName, valueToCheckFor } };
+
+            var output = new List<ProcessedLogEvent>();
+            var logger = new Logger(logEvents => logEvents
+                .Select(BasicFormatting.Process)
+                .Select(FieldNameFormatting.ToSnakeCase())
+                .Subscribe(log => output.Add(log))
+                );
+
+            logger.Info(obj);
+
+            var field = output[0].Content.First();
+
+            Assert.Equal(finalFieldName, field.Key);
+            Assert.Equal(valueToCheckFor, field.Value);
+        }
+
+        [Theory]
+        [InlineData("field", "Field")]
+        [InlineData("Field", "Field")]
+        [InlineData("Field Name", "FieldName")]
+        [InlineData("FieldName", "FieldName")]
+        [InlineData("fieldName", "FieldName")]
+        [InlineData("field_Name", "FieldName")]
+        [InlineData("field_name", "FieldName")]
+        [InlineData("Field_name", "FieldName")]
+        [InlineData("FieldName_Goes here", "FieldNameGoesHere")]
+        //    [InlineData("hotelID", "hotelId")]
+        [InlineData("hotelId", "HotelId")]
+        public void should_convert_fieldnames_to_pascalcase(string originalFieldName, string finalFieldName)
+        {
+            const string valueToCheckFor = "valuetocheckfor";
+
+            var obj = new Dictionary<string, object> { { originalFieldName, valueToCheckFor } };
+
+            var output = new List<ProcessedLogEvent>();
+            var logger = new Logger(logEvents => logEvents
+                .Select(BasicFormatting.Process)
+                .Select(FieldNameFormatting.ToPascalCase())
+                .Subscribe(log => output.Add(log))
+                );
+
+            logger.Info(obj);
+
+            var field = output[0].Content.First();
+
+            Assert.Equal(finalFieldName, field.Key);
+            Assert.Equal(valueToCheckFor, field.Value);
+        }
+
+        [Theory]
+        [InlineData("field", "field")]
+        [InlineData("Field", "field")]
+        [InlineData("Field Name", "fieldName")]
+        [InlineData("FieldName", "fieldName")]
+        [InlineData("fieldName", "fieldName")]
+        [InlineData("field_Name", "fieldName")]
+        [InlineData("field_name", "fieldName")]
+        [InlineData("Field_name", "fieldName")]
+        [InlineData("FieldName_Goes here", "fieldNameGoesHere")]
+        //    [InlineData("hotelID", "hotelId")]
+        [InlineData("hotelId", "hotelId")]
+        public void should_convert_fieldnames_to_camelcase(string originalFieldName, string finalFieldName)
+        {
+            const string valueToCheckFor = "valuetocheckfor";
+
+            var obj = new Dictionary<string, object> { { originalFieldName, valueToCheckFor } };
+
+            var output = new List<ProcessedLogEvent>();
+            var logger = new Logger(logEvents => logEvents
+                .Select(BasicFormatting.Process)
+                .Select(FieldNameFormatting.ToCamelCase())
+                .Subscribe(log => output.Add(log))
+                );
+
+            logger.Info(obj);
+
+            var field = output[0].Content.First();
+
+            Assert.Equal(finalFieldName, field.Key);
+            Assert.Equal(valueToCheckFor, field.Value);
+        }
+    }
+}
+
