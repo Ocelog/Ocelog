080a2d0fe206800d18d59ef912efc5c261e1ff00
Andy Lowry <andy@andylowry.me.uk>
2016-03-22 15:18:19 +0000

    Add support for building against net45

--- package/Package.bat
+++ package/Package.bat
@@ -16,4 +16,16 @@ copy ..\src\Ocelog.Formatting.Logstash\bin\Debug-46\Ocelog.Formatting.Logstash.p
 copy ..\src\Ocelog.Transport.UDP\bin\Debug-46\Ocelog.Transport.UDP.pdb lib\net46
 copy ..\src\Ocelog.Testing\bin\Debug-46\Ocelog.Testing.pdb lib\net46
 
+copy ..\src\Ocelog\bin\Debug-45\Ocelog.dll lib\net45
+copy ..\src\Ocelog.Formatting.Json\bin\Debug-45\Ocelog.Formatting.Json.dll lib\net45
+copy ..\src\Ocelog.Formatting.Logstash\bin\Debug-45\Ocelog.Formatting.Logstash.dll lib\net45
+copy ..\src\Ocelog.Transport.UDP\bin\Debug-45\Ocelog.Transport.UDP.dll lib\net45
+copy ..\src\Ocelog.Testing\bin\Debug-45\Ocelog.Testing.dll lib\net45
+
+copy ..\src\Ocelog\bin\Debug-45\Ocelog.pdb lib\net45
+copy ..\src\Ocelog.Formatting.Json\bin\Debug-45\Ocelog.Formatting.Json.pdb lib\net45
+copy ..\src\Ocelog.Formatting.Logstash\bin\Debug-45\Ocelog.Formatting.Logstash.pdb lib\net45
+copy ..\src\Ocelog.Transport.UDP\bin\Debug-45\Ocelog.Transport.UDP.pdb lib\net45
+copy ..\src\Ocelog.Testing\bin\Debug-45\Ocelog.Testing.pdb lib\net45
+
 nuget pack -Symbols
