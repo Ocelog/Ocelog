20248c1be94982d16b039254083f7af0590b0867
Jason Holloway <miceeatnicerice@hotmail.com>
2018-03-20 00:09:14 +0000

    nuspec and package.bat updated to pack both frameworks

--- package/Package.bat
+++ package/Package.bat
@@ -1,32 +1,32 @@
 mkdir lib
-mkdir lib\net46
+mkdir lib\netstandard2.0
 mkdir lib\net45
 mkdir tools
 mkdir content
 mkdir content\controllers
 
-copy ..\src\Ocelog\bin\Debug-46\Ocelog.dll lib\net46
-copy ..\src\Ocelog.Formatting.Json\bin\Debug-46\Ocelog.Formatting.Json.dll lib\net46
-copy ..\src\Ocelog.Formatting.Logstash\bin\Debug-46\Ocelog.Formatting.Logstash.dll lib\net46
-copy ..\src\Ocelog.Transport.UDP\bin\Debug-46\Ocelog.Transport.UDP.dll lib\net46
-copy ..\src\Ocelog.Testing\bin\Debug-46\Ocelog.Testing.dll lib\net46
+copy ..\src\Ocelog\bin\Debug\netstandard2.0\Ocelog.dll lib\netstandard2.0
+copy ..\src\Ocelog.Formatting.Json\bin\Debug\netstandard2.0\Ocelog.Formatting.Json.dll lib\netstandard2.0
+copy ..\src\Ocelog.Formatting.Logstash\bin\Debug\netstandard2.0\Ocelog.Formatting.Logstash.dll lib\netstandard2.0
+copy ..\src\Ocelog.Transport.UDP\bin\netstandard2.0\Ocelog.Transport.UDP.dll lib\netstandard2.0
+copy ..\src\Ocelog.Testing\bin\netstandard2.0\Ocelog.Testing.dll lib\netstandard2.0
 
-copy ..\src\Ocelog\bin\Debug-46\Ocelog.pdb lib\net46
-copy ..\src\Ocelog.Formatting.Json\bin\Debug-46\Ocelog.Formatting.Json.pdb lib\net46
-copy ..\src\Ocelog.Formatting.Logstash\bin\Debug-46\Ocelog.Formatting.Logstash.pdb lib\net46
-copy ..\src\Ocelog.Transport.UDP\bin\Debug-46\Ocelog.Transport.UDP.pdb lib\net46
-copy ..\src\Ocelog.Testing\bin\Debug-46\Ocelog.Testing.pdb lib\net46
+copy ..\src\Ocelog\bin\Debug\netstandard2.0\Ocelog.pdb lib\netstandard2.0
+copy ..\src\Ocelog.Formatting.Json\bin\Debug\netstandard2.0\Ocelog.Formatting.Json.pdb lib\netstandard2.0
+copy ..\src\Ocelog.Formatting.Logstash\bin\Debug\netstandard2.0\Ocelog.Formatting.Logstash.pdb lib\netstandard2.0
+copy ..\src\Ocelog.Transport.UDP\bin\Debug\netstandard2.0\Ocelog.Transport.UDP.pdb lib\netstandard2.0
+copy ..\src\Ocelog.Testing\bin\Debug\netstandard2.0\Ocelog.Testing.pdb lib\netstandard2.0
 
-copy ..\src\Ocelog\bin\Debug-45\Ocelog.dll lib\net45
-copy ..\src\Ocelog.Formatting.Json\bin\Debug-45\Ocelog.Formatting.Json.dll lib\net45
-copy ..\src\Ocelog.Formatting.Logstash\bin\Debug-45\Ocelog.Formatting.Logstash.dll lib\net45
-copy ..\src\Ocelog.Transport.UDP\bin\Debug-45\Ocelog.Transport.UDP.dll lib\net45
-copy ..\src\Ocelog.Testing\bin\Debug-45\Ocelog.Testing.dll lib\net45
+copy ..\src\Ocelog\bin\Debug\net45\Ocelog.dll lib\net45
+copy ..\src\Ocelog.Formatting.Json\bin\Debug\net45\Ocelog.Formatting.Json.dll lib\net45
+copy ..\src\Ocelog.Formatting.Logstash\bin\Debug\net45\Ocelog.Formatting.Logstash.dll lib\net45
+copy ..\src\Ocelog.Transport.UDP\bin\Debug\net45\Ocelog.Transport.UDP.dll lib\net45
+copy ..\src\Ocelog.Testing\bin\Debug\net45\Ocelog.Testing.dll lib\net45
 
-copy ..\src\Ocelog\bin\Debug-45\Ocelog.pdb lib\net45
-copy ..\src\Ocelog.Formatting.Json\bin\Debug-45\Ocelog.Formatting.Json.pdb lib\net45
-copy ..\src\Ocelog.Formatting.Logstash\bin\Debug-45\Ocelog.Formatting.Logstash.pdb lib\net45
-copy ..\src\Ocelog.Transport.UDP\bin\Debug-45\Ocelog.Transport.UDP.pdb lib\net45
-copy ..\src\Ocelog.Testing\bin\Debug-45\Ocelog.Testing.pdb lib\net45
+copy ..\src\Ocelog\bin\Debug\net45\Ocelog.pdb lib\net45
+copy ..\src\Ocelog.Formatting.Json\bin\Debug\net45\Ocelog.Formatting.Json.pdb lib\net45
+copy ..\src\Ocelog.Formatting.Logstash\bin\Debug\net45\Ocelog.Formatting.Logstash.pdb lib\net45
+copy ..\src\Ocelog.Transport.UDP\bin\Debug\net45\Ocelog.Transport.UDP.pdb lib\net45
+copy ..\src\Ocelog.Testing\bin\Debug\net45\Ocelog.Testing.pdb lib\net45
 
 nuget pack -Symbols
